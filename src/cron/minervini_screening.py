142093fd26d7c243c583f2725663e1c67064947a
Junghoon <brighter87@naver.com>
2025-09-30 22:01:55 +0900

    a

---
+++ src/cron/minervini_screening.py
@@ -0,0 +1,20 @@
+from src.analysis.prepare import get_ohlc_ma_rs_for_analysis, save_ohlc_ma_rs_parquet
+from src.analysis.screen_minervini import screen_minervini
+from src.data.dbs.setup_db import save_upsert
+from src.data.get_from_web import get_ohlc_all_from_web, write_tickerslist
+from src.utils.date import get_last_business_day
+
+
+def main():
+    """오늘이 공휴일인지 아닌지 확인하고 공휴일이 아닌 경우에만 실행"""
+    write_tickerslist()
+    get_ohlc_all_from_web(days=10)
+    save_ohlc_ma_rs_parquet()
+    last_business_day = get_last_business_day(as_string=True)
+    df = get_ohlc_ma_rs_for_analysis(date=last_business_day)
+    result_df = screen_minervini(df, rs_col="RS_200_pct")
+    save_upsert(result_df)
+
+
+if __name__ == "__main__":
+    main()
--- src/utils/date.py
+++ src/utils/date.py
@@ -2,13 +2,48 @@ import pandas as pd
 import pandas_market_calendars as mcal
 
 
-def get_datelist(days=0, today=pd.Timestamp.today(tz="UTC")) -> pd.Timestamp:
+def get_datelist(
+    days=0,
+    today=pd.Timestamp.today(tz="America/New_York"),
+) -> pd.Timestamp:
+
+    end_date = get_last_business_day()
+    print("Date List를 만드는 중...")
+
     nyse = mcal.get_calendar("NYSE")
+
     day_range = nyse.schedule(
-        start_date=today - pd.Timedelta(days=days * 2),
-        end_date=today,
+        start_date=end_date - pd.Timedelta(days=days * 2),
+        end_date=end_date,
     )
+
     return [_to_string_from_time(date) for date in day_range["market_open"]]
+
+
+def get_last_business_day(as_string=False):
+    nyse = mcal.get_calendar(name="NYSE")
+    today = pd.Timestamp.today(tz="America/New_York")
+    market_close = pd.Timestamp(
+        year=today.year,
+        month=today.month,
+        day=today.day,
+        hour=16,
+        minute=0,
+        tz="America/New_York",
+    )
+    if today >= market_close:
+        end_date = today
+    else:
+        end_date = today - pd.Timedelta(days=1)
+
+    last_business_day = nyse.schedule(
+        start_date=end_date - pd.Timedelta(days=10),
+        end_date=end_date,
+    ).index[-1]
+
+    if as_string:
+        return last_business_day.strftime("%Y-%m-%d")
+    return last_business_day
 
 
 def get_today_as_string():

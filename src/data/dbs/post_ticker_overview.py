1a239b0eca1bfbf8871e46b44998bbb8ae538b01
Junghoon <brighter87@naver.com>
2025-10-01 15:44:21 +0900

    a

--- src/data/dbs/post_ticker_overview.py
+++ src/data/dbs/post_ticker_overview.py
@@ -4,8 +4,9 @@ from datetime import date
 
 import numpy as np
 import pandas as pd
-from sqlalchemy import BIGINT, DECIMAL, Date, Index, Integer, String, UniqueConstraint
+from sqlalchemy import BIGINT, JSON, Date, Index, Integer, String
 from sqlalchemy.dialects.mysql import insert as mysql_insert
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column
 from src.data.dbs.setup_db import ENGINE
 from src.data.get_from_local import read_ticker_overviews_from_local
@@ -16,24 +17,66 @@ class Base(DeclarativeBase):
 
 
 class Ticker_Overview_US(Base):
-    """전체 수정해야 함.
-    """
+    """전체 수정해야 함."""
 
-    __tablename__ = "ticer_overview_us"
+    __tablename__ = "ticker_overview_us"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    T: Mapped[str] = mapped_column(String(10), nullable=False)
-    close_date: Mapped[date] = mapped_column(Date, nullable=True)
-    open: Mapped[float] = mapped_column(DECIMAL(10, 4), nullable=True)
-    close: Mapped[float] = mapped_column(DECIMAL(10, 4), nullable=True)
-    volume: Mapped[int] = mapped_column(BIGINT, nullable=True)
-    high: Mapped[float] = mapped_column(DECIMAL(10, 4), nullable=True)
-    low: Mapped[float] = mapped_column(DECIMAL(10, 4), nullable=True)
-    vw: Mapped[float] = mapped_column(DECIMAL(10, 4), nullable=True)
-    n: Mapped[int] = mapped_column(Integer, nullable=True)
+    T: Mapped[str] = mapped_column(
+        String(10),
+        nullable=False,
+        unique=True,
+    )  # ticker -> T
+    name: Mapped[str] = mapped_column(String(255), nullable=True)
+    market: Mapped[str] = mapped_column(String(20), nullable=True)
+    locale: Mapped[str] = mapped_column(String(10), nullable=True)
+    primary_exchange: Mapped[str] = mapped_column(String(10), nullable=True)
+    stock_type: Mapped[str] = mapped_column(String(10), nullable=True)
+    active: Mapped[str] = mapped_column(String(10), nullable=True)
+    currency_name: Mapped[str] = mapped_column(String(10), nullable=True)
+    cik: Mapped[str] = mapped_column(String(10), nullable=True)
+    market_cap: Mapped[int] = mapped_column(BIGINT, nullable=True)
+    description: Mapped[str] = mapped_column(String(1000), nullable=True)
+    homepage_url: Mapped[str] = mapped_column(String(255), nullable=True)
+    list_date: Mapped[date] = mapped_column(Date, nullable=True)
+    branding: Mapped[dict] = mapped_column(JSON, nullable=True)
+    share_class_shares_outstanding: Mapped[int] = mapped_column(BIGINT, nullable=True)
+    weighted_shares_outstanding: Mapped[int] = mapped_column(BIGINT, nullable=True)
+    round_lot: Mapped[int] = mapped_column(Integer, nullable=True)
+    composite_figi: Mapped[str] = mapped_column(String(12), nullable=True)
+    share_class_figi: Mapped[str] = mapped_column(String(12), nullable=True)
+    phone_number: Mapped[str] = mapped_column(String(20), nullable=True)
+    address: Mapped[dict] = mapped_column(JSON, nullable=True)
+    sic_code: Mapped[str] = mapped_column(String(4), nullable=True)
+    sic_description: Mapped[str] = mapped_column(String(100), nullable=True)
+    total_employees: Mapped[int] = mapped_column(BIGINT, nullable=True)
+    description_ko: Mapped[str] = mapped_column(String(1000), nullable=True)
 
     __table_args__ = (
-        UniqueConstraint("T", "close_date", name="uq_T_date"),
-        Index("T", "close_date", "close", "volume", "vw", "n"),
+        Index(
+            "idx_basic",
+            "name",
+            "market",
+            "locale",
+            "primary_exchange",
+            "stock_type",
+        ),
+        Index(
+            "list_date",
+            "share_class_shares_outstanding",
+            "weighted_shares_outstanding",
+        ),
+        Index("figi_code", "composite_figi", "share_class_figi"),
+        Index("idx_sic", "sic_code", "sic_description"),
+        Index(
+            "ft_description",
+            "description",
+            mysql_prefix="FULLTEXT",
+        ),
+        Index(
+            "ft_description_ko",
+            "description_ko",
+            mysql_prefix="FULLTEXT",
+        ),
     )
 
 
@@ -48,16 +91,33 @@ def save_upsert(
     mapping: Mapping[str:str] | None = None,
     chunk_size: int = 1000,
 ) -> int:
+    """ "ticker","""
     default_mapping = {
-        "T": "T",
-        "close_date": "date",
-        "open": "o",
-        "high": "h",
-        "low": "l",
-        "close": "c",
-        "volume": "v",
-        "vw": "vw",
-        "n": "n",
+        "T": "ticker",
+        "name": "name",
+        "market": "market",
+        "locale": "locale",
+        "primary_exchange": "primary_exchange",
+        "stock_type": "type",
+        "active": "active",
+        "currency_name": "currency_name",
+        "cik": "cik",
+        "market_cap": "market_cap",
+        "description": "description",
+        "homepage_url": "homepage_url",
+        "list_date": "list_date",
+        "branding": "branding",
+        "share_class_shares_outstanding": "share_class_shares_outstanding",
+        "weighted_shares_outstanding": "weighted_shares_outstanding",
+        "round_lot": "round_lot",
+        "composite_figi": "composite_figi",
+        "share_class_figi": "share_class_figi",
+        "phone_number": "phone_number",
+        "address": "address",
+        "sic_code": "sic_code",
+        "sic_description": "sic_description",
+        "total_employees": "total_employees",
+        "description_ko": "description_ko",
     }
     if mapping:
         default_mapping.update(mapping)
@@ -65,7 +125,7 @@ def save_upsert(
     cols = default_mapping.values()
     df = df[cols].copy()
 
-    df["date"] = pd.to_datetime(df["date"]).dt.date
+    df["list_date"] = pd.to_datetime(df["list_date"]).dt.date
     df = df.replace({np.nan: None})
 
     rows = (
@@ -77,11 +137,11 @@ def save_upsert(
     with Session(ENGINE) as s, s.begin():
         for i in range(0, len(rows), chunk_size):
             batch = rows[i : i + chunk_size]
-            stmt = mysql_insert(OHLC_US).values(batch)
+            stmt = mysql_insert(Ticker_Overview_US).values(batch)
 
             update_cols = {
                 c.name: stmt.inserted[c.name]
-                for c in OHLC_US.__table__.columns
+                for c in Ticker_Overview_US.__table__.columns
                 if c.name not in ("id",)
             }
             stmt = stmt.on_duplicate_key_update(**update_cols)
@@ -97,9 +157,12 @@ def post_ohlc_from_local():
 
 def main():
     df = read_ticker_overviews_from_local(lang="ko")
-    breakpoint()
     init_schema()
-    save_upsert(df)
+    try:
+        save_upsert(df)
+    except IntegrityError as e:
+        print("IntegrityError:", e.orig)  # MySQL/MariaDB의 구체 메시지
+        raise
 
 
 if __name__ == "__main__":
